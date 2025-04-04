c9b1e3ef67de302bfe916496b04b556d31cded51
Damir <159621868+nchsvj@users.noreply.github.com>
2025-04-04 17:47:45 +0500

    Delete app directory

--- app/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql+asyncpg://postgres:123@localhost/postgres"
-
-async_engine = create_async_engine(DATABASE_URL, echo=True)
-Base = declarative_base()
-async_session_maker = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
